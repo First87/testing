97054b05e1265140774a115bdeba603dc205b25e
First87 <127948958+First87@users.noreply.github.com>
2023-09-12 20:26:11 +0700

    Update CardProduct.tsx

--- src/pages/components/CardProduct.tsx
+++ src/pages/components/CardProduct.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
@@ -127,23 +126,21 @@ export default function Example() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl text-center font-bold tracking-tight text-gray-900">สินค้าของเรา555</h2>
-         <div styles={{ boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }}>
+        <h2 className="text-2xl text-center font-bold tracking-tight text-gray-900">สินค้าของเรา</h2>
+
         <div className="mt-6 md:grid md:gap-6 gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-4 mb-12 ">
                            
-                        
+                           
           {products.map((product) => (
             <div key={product.id} className="group relative">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              
-              <Image
+                <Image
                   src={product.imageSrc}
                   alt={product.imageAlt}
                   width={200}
                   height={400}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
-            
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
@@ -158,8 +155,6 @@ export default function Example() {
                 <p className="text-sm font-medium text-gray-900">{product.price}</p>
               </div>
             </div>
-            
-               
           ))}
         </div>
       </div>
