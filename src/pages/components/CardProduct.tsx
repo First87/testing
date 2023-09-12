7f152943becec523c2bc311f43fb47d93165f826
First87 <127948958+First87@users.noreply.github.com>
2023-09-11 22:17:45 -0700

    Update CardProduct.tsx

--- src/pages/components/CardProduct.tsx
+++ src/pages/components/CardProduct.tsx
@@ -134,13 +134,15 @@ export default function Example() {
           {products.map((product) => (
             <div key={product.id} className="group relative">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <Image
+              <div>
+              <Image
                   src={product.imageSrc}
                   alt={product.imageAlt}
                   width={200}
                   height={400}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
+              </div>
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
