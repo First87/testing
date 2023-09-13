b4475e28d6c41dba070bcc51838c67b9da2c2919
First87 <127948958+First87@users.noreply.github.com>
2023-09-13 13:18:03 +0700

    Update Layout.tsx

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -1,5 +1,5 @@
 
-import Footer from "../component/Footer";
+import Footer from "./component/Footer";
 import React, { ReactNode } from 'react';
 import Header from './components/Header';
 interface LayoutProps {
