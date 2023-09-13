2eb3d503d4aa91c3b96bc73370af2fba58ee8d4f
First87 <127948958+First87@users.noreply.github.com>
2023-09-13 13:13:27 +0700

    Create Layout.tsx

---
+++ src/pages/components/Layout.tsx
@@ -0,0 +1,22 @@
+import NavBar from "../component/NavBar";
+import Footer from "../component/Footer";
+import React, { ReactNode } from 'react';
+import Header from './components/Header';
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div>
+      <div>
+       <Header />
+        <br /><br />
+      </div>
+      <div>{children}</div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
