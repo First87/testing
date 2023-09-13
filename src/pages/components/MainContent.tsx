b2a5dabd3143f605413adc4b0ef0afbcedd0ddbf
First87 <127948958+First87@users.noreply.github.com>
2023-09-13 13:42:08 +0700

    Update MainContent.tsx

--- src/pages/components/MainContent.tsx
+++ src/pages/components/MainContent.tsx
@@ -7,7 +7,7 @@ export default function Example() {
           
           <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
             <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
-              เกี่ยวกับเรา
+              เกี่ยวกับเรา555555555
             </h2>
             <div className="line"></div>
 
