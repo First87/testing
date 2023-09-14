eb97918a8dabe097b464ffa104171423f15b6ca4
First87 <apisitpanya5@gmail.com>
2023-09-14 20:18:53 +0700

    update Navbar

--- src/pages/components/MainContent.tsx
+++ src/pages/components/MainContent.tsx
@@ -6,13 +6,14 @@ export default function Example() {
         <div className="relative isolate overflow-hidden  px-6 pt-16  sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:space-x-20">
           
           <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-            <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
-              เกี่ยวกับเรา555555555
+            <h2 className="text-2xl font-bold tracking-tight text-black sm:text-4xl">
+            เสริมศิริ อลูมินั่ม & สแตนเลส
             </h2>
-            <div className="line"></div>
+            <div className="line"
+            style={{margin:"2rem auto"}}></div>
 
             <p className="mt-6 text-lg leading-8 text-black">
-              เสริมศิริ อลูมินั่ม & สแตนเลส ยินดีต้อนรับ ลูกค้าทุกท่านค่ะ
+               ยินดีต้อนรับ ลูกค้าทุกท่านค่ะ
               เราเป็นศูนย์รวมอลูมิเนียมเส้น และสแตนเลส
               อุปกรณ์งานติดตั้งประตู-หน้าต่างกรอบอลูมิเนียมที่เกี่ยวข้องทุกชนิดค่ะ
               จัดส่งได้ทั่วประเทศ สามารถทักมาก่อนได้ line : 086-569-2289 หรือ
--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,33 +1,34 @@
-import Image from 'next/image';
-import Link from 'next/link'; // Import the Link component from Next.js for client-side navigation.
-import { useState } from 'react'; // Import the useState hook from React for managing state.
+import Image from "next/image";
+import Link from "next/link"; // Import the Link component from Next.js for client-side navigation.
+import { useState } from "react"; // Import the useState hook from React for managing state.
 
 export default function Navbar() {
   const [navbar, setNavbar] = useState(false); // Initialize a state variable 'navbar' and a function 'setNavbar' to manage it, with an initial value of 'false'.
 
   return (
     <div>
-  
-      <nav className="w-full bg-gray-800 shadow"> {/* Create a navigation bar with background color and shadow */}
-        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8"> {/* Set the layout and spacing */}
-        <Image
-                    src="/nav.png"
-                    height={60}
-                    width={60}
-                    alt='logo'
-                    />
-          <div>
-            
-            <div className="flex items-center justify-between py-3 md:py-5 md:block"> {/* Create a flex container for the logo and menu items */}
-              
-              
-              <Link href="#">
-              
-              
-                    <h2 className="font-bold logo-text">บริษัท เสริมศิริ อลูมินั่ม & สแตนเลส จำกัด</h2>
-              </Link>
+      <nav className="w-full bg-gray-800 shadow">
+        {" "}
+        {/* Create a navigation bar with background color and shadow */}
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+          {" "}
+          {/* Set the layout and spacing */}
+          <div className="mobile-nav flex justify-between gap-5  ">
+            <Image src="/nav.png" height={60} width={60} alt="logo"
+            className="object-contain object-center"
+            />
+            <div>
+              <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                {" "}
+                {/* Create a flex container for the logo and menu items */}
+                <Link href="#">
+                  <h2 className="font-bold logo-text">
+                    บริษัท เสริมศิริ อลูมินั่ม & สแตนเลส จำกัด
+                  </h2>
+                </Link>
+              </div>
+              </div>
 
-              
               <div className="md:hidden">
                 <button
                   className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
@@ -65,41 +66,35 @@ export default function Navbar() {
                 </button>
               </div>
             </div>
-            
-          </div>
+          
           <div>
             <div
               className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? 'block' : 'hidden'
+                navbar ? "block" : "hidden"
               }`}
-            > {/* Create a menu that is hidden on larger screens */}
-              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0"> {/* Create a list of navigation links */}
+            >
+              {" "}
+              {/* Create a menu that is hidden on larger screens */}
+              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                {" "}
+                {/* Create a list of navigation links */}
                 <li className="text-white">
-                  <Link href="/">
-                    หน้าแรก
-                  </Link>
+                  <Link href="/">หน้าแรก</Link>
                 </li>
                 <li className="text-white">
-                  <Link href="/products">
-                    สินค้าของเรา
-                  </Link>
+                  <Link href="/products">สินค้าของเรา</Link>
                 </li>
                 <li className="text-white">
-                  <Link href="/about">
-                    สาระน่ารู้
-                  </Link>
+                  <Link href="/about">สาระน่ารู้</Link>
                 </li>
                 <li className="text-white">
-                  <Link href="/contact">
-                    ติดต่อเรา
-                  </Link>
+                  <Link href="/contact">ติดต่อเรา</Link>
                 </li>
               </ul>
             </div>
           </div>
         </div>
       </nav>
-      
     </div>
   );
 }
