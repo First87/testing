eb44a8d283d71aa0221321382269682f80b214a6
First87 <127948958+First87@users.noreply.github.com>
2023-09-28 18:10:36 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Banner from './components/Banner';
+// import Banner from './components/Banner';
 import MainContent from './components/MainContent';
 import  MainSlide from './components/MainSlide';
 import Head from 'next/head';
